e6e0b4bc47c383123a71d90d143beae4223001f4
Ahmed Alsuwaidi <aasuwaidi24@gmail.com>
2018-12-29 21:50:22 +0000

    Removed files

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-var webpack = require("webpack");
-module.exports = {
-	entry: "./app.js",
-	output: {
-		filename: "background.js"
-	},
-	plugins: [new webpack.DefinePlugin({"process.env": {LATER_COV: false}})]
-};
